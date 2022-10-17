825f8884ce8418158a28bc0fd5d06a020af12836
Sal Carrera <=>
2022-10-16 21:36:07 -0400

    practice

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,6 @@
 <script>
 	import { onMount } from 'svelte';
-
 	const obj ={'test': 'string'};
-
-
 	switch(obj) {
 		case 'sal': 
 			console.log('stuff');
@@ -11,7 +8,8 @@
  // test
 	if (x === x) {
 
-	}
+	     }
+
 
 	export let name;
 	const s = 12;
@@ -25,7 +23,24 @@
 </script>
 
 <main>
-	
+	<div>
+		<ul>
+			<li>sal</li>
+		</ul>
+	</div>
+	ul>li.item$@-3*5
+	<ul>
+		<li class="item12">asdf</li>
+		<li class="item11">asdf</li>
+		<li class="item10"></li>
+		<li class="item9"><asdfa/li>
+		<li class="item8"></li>
+		<li class="item7"></li>
+		<li class="item6"></li>
+		<li class="item5"></li>
+		<li class="item4"></li>
+		<li class="item3"></li>
+	</ul>
 </main>
 
 <style>
